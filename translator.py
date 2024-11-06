b4759cc21994f0e5ba3cb1caead2e86d8e5a36a2
Arjun Choudhary <u1103050@internal.imsglobal.com>
2024-11-06 14:28:25 +0530

    first commit

---
+++ translator.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+from googletrans import Translator
+import os
+
+
+st.title('Translation App')
+languages = {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'he': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'or': 'odia', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'ug': 'uyghur', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu'}
+source_lang = st.selectbox(
+    'Select a language',
+    index=None,
+    placeholder="Select contact method...",
+    options=list(languages.keys()),  # Language codes as options
+    format_func=lambda x: languages[x]  # Display language names in the dropdown
+)
+
+target_lang = 'en'
+if source_lang is not None:
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        with open(uploaded_file.name, 'wb') as f:
+            f.write(uploaded_file.getbuffer()) 
+        translator = Translator()
+        if source_lang is not None and os.path.exists(uploaded_file.name):
+            excel_data_df = pd.read_excel(uploaded_file.name,header = None)
+            i = 0
+            newexceldata = []
+            for string in excel_data_df[0].tolist():
+                if i == 0:
+                    i = i + 1    
+                    continue
+                text_to_translate = translator.translate(string, src=source_lang, dest=target_lang)
+                translated_text = text_to_translate.text
+                newexceldata.append([string,translated_text])
+            df = pd.DataFrame(newexceldata) 
+            df.columns = ['Description(French)', 'English Translation']    
+            df = df.reset_index(drop=True)
+            st.write(df)
+            df.to_excel(uploaded_file.name,'Sheet1',index=False)
+            
